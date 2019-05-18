fba8540c3015a919839d5fd252b0b399e2e36f1b
unknown <cateran1995@gmail.com>
2019-05-18 16:06:05 -0700

    <Polished Psuedo Code Final>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,25 @@
 // Psuedo Code
+
 // We need to generate a Random Number on the screen.
 
-// This Random Number needs to be in between 19-120
+// This Random Number needs to be in between 19-120.
 
 // We need Four Crystals that act as buttons.
 
 // Every Crystal is attached a random number between 1-12.
 
-// Every time we win or lose, a new random number is
-// attached to each respective crystal.
-
-// Each time we click on a Crystal, it should be adding
-// itself to the previous chosen Crystals number value.
+// Each time we click on a Crystal, it should be adding the 
+// curent Crystal value to the Crystals number value chosen // prior.
 
 // Crystal Values are added until your score equals that 
 // of the Random Number.
 
-// If it is not equal, we lose and begin over again.
+// If it is greater than Random Result, we lose and loss
+// is counted against us.
 
 // If it is equal, we win and count it as a win.
+
+// Every time we win or lose, a new random number is
+// attached to each respective crystal.
+
+// After each round a new Random Number is also Generated.
