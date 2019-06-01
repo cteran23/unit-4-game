52cd9c5b053b33c1717066c615ea53345c4efd1f
unknown <cateran1995@gmail.com>
2019-06-01 16:30:58 -0700

    <Added Images and CSS Properties>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,17 @@ var lost = 0;
 var win = 0;
 var previous = 0;
 
-var startAndReset = function() {
+var startAndReset = function () {
   $(".crystals").empty();
 
-  randomResult = Math.floor(Math.random() * 101) + 19;
+  var images = [
+    'https://i.pinimg.com/originals/bd/4e/0c/bd4e0c1db2341cb53a73f001abbbcbe5.jpg',
+    'https://media.glamour.com/photos/58fa47ebe2ef792952c30dca/1:1/w_1578,h_1578,c_limit/crystalriver.jpg',
+    'https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/10/Arena_Crystal.png/revision/latest?cb=20150825214845',
+    'https://images.unsplash.com/photo-1546608135-e5de34abc308?ixlib=rb-1.2.1&w=1000&q=80'
+  ]
 
+  randomResult = Math.floor(Math.random() * 101) + 19;
   $("#result").html("Random Result: " + randomResult);
 
   for (var i = 0; i < 4; i++) {
@@ -19,21 +25,28 @@ var startAndReset = function() {
       "result-random": random
     });
 
+    crystal.css({
+      "background-image":"url('" + images[i] + "')",
+      "background-size":"cover"
+    })
+
     $(".crystals").append(crystal);
-    
+
   }
 
-  $("#previous").html("Total Score: ", + previous);
+  $("#previous").html("Total Score: " + previous);
 
 };
 
+
+
 startAndReset();
 
-$(document).on("click", ".crystal", function() {
+$(document).on("click", ".crystal", function () {
   var num = parseInt($(this).attr("result-random"));
   previous += num;
 
-  $("#previous").html("Total Score: ", + previous);
+  $("#previous").html("Total Score: " + previous);
 
   console.log(previous);
 
@@ -41,15 +54,11 @@ $(document).on("click", ".crystal", function() {
     lost++;
     $("#lost").html("You Lost: " + lost);
     previous = 0;
-
-
     startAndReset();
+
   } else if (previous === randomResult) {
-
     win++;
-
     $("#win").html("You Win: " + win);
-
     previous = 0;
     startAndReset();
   }
